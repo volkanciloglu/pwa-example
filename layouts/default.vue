3d294d10f79bf0bda395b4c0e74738619d647b61
Volkan <volkanciloglu@gmail.com>
2020-08-03 19:16:06 +0300

    de

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,18 +6,13 @@
 <script>
 export default {
   mounted() {
-    // Inside page components
-    this.$OneSignal.push(() => {
-      this.$OneSignal.isPushNotificationsEnabled(isEnabled => {
-        if (isEnabled) {
-          console.log("Push notifications are enabled!");
-        } else {
-          console.log("Push notifications are not enabled yet.");
-        }
+    this.$OneSignal.push(async () => {
+      await this.$OneSignal.isPushNotificationsEnabled(isEnabled => {
+        if (isEnabled) console.log("Push notifications are enabled!");
+        else console.log("Push notifications are not enabled yet.");
       });
     });
 
-    // Using window and array form
     window.$OneSignal.push([
       "addListenerForNotificationOpened",
       data => {
